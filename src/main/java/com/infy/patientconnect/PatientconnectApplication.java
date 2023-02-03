6232d60992e1b768f4ee95934e646fdd7dadc629
Piyush7034 <piyushshukla2100@gmail.com>
2023-02-03 20:23:59 +0530

    Initial Commit

---
+++ src/main/java/com/infy/patientconnect/PatientconnectApplication.java
@@ -0,0 +1,13 @@
+package com.infy.patientconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infy/patientconnect/PatientconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.patientconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
